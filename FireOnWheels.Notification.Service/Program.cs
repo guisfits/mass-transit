59bff74dd4aac0124fda3a4297146ae9e6c0c112
Guilherme Camargo <guisfits@hotmail.com>
2020-08-25 20:56:55 -0300

    notifications

---
+++ FireOnWheels.Notification.Service/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using FireOnWheels.Messaging;
+using MassTransit;
+
+namespace FireOnWheels.Notification.Service
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.Title = "Notification";
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(config =>
+            {
+                config.ReceiveEndpoint(RabbitMqConstants.NotificationServiceQueue, e =>
+                {
+                    e.Consumer<OrderRegisteredConsumer>();
+                });
+            });
+
+            await bus.StartAsync();
+
+            Console.WriteLine("Listening for Order registered events.. Press enter to exit");
+            Console.ReadLine();
+
+            await bus.StopAsync();
+        }
+    }
+}
