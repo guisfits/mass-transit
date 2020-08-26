8f572a531c750ea4e83e91d42831f7771425d31c
Guilherme Camargo <guisfits@hotmail.com>
2020-08-25 22:14:19 -0300

    API

---
+++ FireOnWheels/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using FireOnWheels.Messaging;
+using FireOnWheels.Registration.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FireOnWheels.Registration.Controllers
+{
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> RegisterOrder(OrderInputModel model)
+        {
+            var bus = BusConfigurator.ConfigureBus();
+
+            var sendToUri = new Uri(RabbitMqConstants.RabbitMqUri + RabbitMqConstants.RegisterOrderServiceQueue);
+            var endPoint = await bus.GetSendEndpoint(sendToUri);
+            await endPoint.Send<IRegisterOrderCommand>(model);
+
+            return Created("", model);
+        }
+    }
+}
