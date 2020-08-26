bcc8dcb909a0a1f7ea2ddec3ac047f4b2e9f163b
Guilherme Camargo <guisfits@hotmail.com>
2020-08-25 22:14:11 -0300

    Finance.Service

---
+++ FireOnWheels.Finance.Service/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using FireOnWheels.Messaging;
+using MassTransit;
+
+namespace FireOnWheels.Finance.Service
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.Title = "Finance";
+
+            var bus = BusConfigurator.ConfigureBus(cfg =>
+            {
+                cfg.ReceiveEndpoint(RabbitMqConstants.FinanceServiceQueue, e =>
+                {
+                    e.Consumer<OrderRegisteredConsumer>();
+                });
+            });
+
+            await bus.StartAsync();
+
+            Console.WriteLine("Listening for Order registered events.. Press enter to exit");
+            Console.ReadLine();
+
+            await bus.StopAsync();
+        }
+    }
+}
