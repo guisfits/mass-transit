23bd922e3fd9ef00e8c2b86c402c874c8cfc6ff8
Guilherme Camargo <guisfits@hotmail.com>
2020-08-25 21:29:24 -0300

    Registration.Service

---
+++ FireOnWheels.Registration.Service/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using FireOnWheels.Messaging;
+using MassTransit;
+
+namespace FireOnWheels.Registration.Service
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.Title = "Registration";
+
+            var bus = BusConfigurator.ConfigureBus(cfg =>
+            {
+                cfg.ReceiveEndpoint(RabbitMqConstants.RegisterOrderServiceQueue, e =>
+                {
+                    e.Consumer<RegisterOrderCommandConsumer>();
+                });
+            });
+
+            await bus.StartAsync();
+
+            Console.WriteLine("Listening for Register order commands.. Press enter to exit");
+            Console.ReadLine();
+
+            await bus.StopAsync();
+        }
+    }
+}
